17de228fa557c9404aea166d6efc415c661dfd13
anas wakach <58094256+madCoder100@users.noreply.github.com>
2024-03-10 23:20:51 +0000

    Update nodeserver.js

--- nodeserver.js
+++ nodeserver.js
@@ -51,5 +51,5 @@ app.get('/', (req, res) => {
 
 // Start the server and make it listen for incoming requests
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    // console.log(`Server is running on http://localhost:${PORT}`);
 });
