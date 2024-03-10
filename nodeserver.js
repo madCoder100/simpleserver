4866d0bec2e4a518be752c0665d41e3597db5d52
anas wakach <58094256+madCoder100@users.noreply.github.com>
2024-03-10 22:33:43 +0000

    Update nodeserver.js

--- nodeserver.js
+++ nodeserver.js
@@ -1,13 +1,13 @@
 const puppeteer = require('puppeteer');
 const express = require('express');
 const bodyParser = require('body-parser')
-// const cors = require('cors');
+const cors = require('cors');
 
 
 const app = express();
 const PORT = 3000;
 
-// app.use(cors());
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
